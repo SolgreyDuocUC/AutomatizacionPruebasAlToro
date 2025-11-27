a6390214acada4cd1a60f81e0e9744d8d1925ae2
Solgrey <sol.medina@duocuc.cl>
2025-11-27 18:38:04 -0300

    Correción y recuperación

---
+++ src/test/java/Ejecutador/Runner.java
@@ -0,0 +1,16 @@
+package Ejecutador;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"./src/test/resources/Features/FeatureFondos.feature"}, glue= {"DefinicionPasos"},
+                 plugin = { "json:target/cucumber.json", "pretty","html:target/cucumber-reports/report.html",
+                             "junit:target/cucumber-results.xml" },
+                               monochrome = true)
+
+public class Runner {
+
+}
